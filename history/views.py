48fb6b3890e40234c797b32e94148708088b9c29
Niranjan B <niranjannb7777@gmail.com>
2020-11-01 16:43:13 +0530

    superuser

--- history/views.py
+++ history/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework import generics
 # Create your views here.
 from .models import History
 from .serializer import HistorySerializer
 
 class HistoryListView(generics.ListAPIView,generics.CreateAPIView):
+    li = []
     serializer_class = HistorySerializer
-    queryset = History.objects.all()
-#
-# class CreateHistory(generics.CreateAPIView):
-#     queryset = History.objects.all()
-#     serializer_class = HistorySerializer
+    queryset = History.objects.all().order_by('-pk')
+
